e8232c61da60d583cadea49deb11562a09a80fc9
Seamey <seameychanntha@26gmail.com>
2024-02-16 18:05:23 +0700

    Homework JavaScript

---
+++ Array.js
@@ -0,0 +1,98 @@
+// syntax array
+let arr = new Array()  // empty array from constructor
+let arr1 = [] // empty array using literal notation
+
+let student = new Array(10,"Panha")
+let teacher = ["Pheaktra","Helen","Sokcheat","leng"]
+
+console.log('array index of array ',teacher[2])
+console.log('total item in array ',teacher.length)
+console.log('access last index of array ', teacher[teacher.length-1])
+
+
+
+let BaseUrl= `https://www.apple.com/autopush/ww/search/modules/`
+// array object
+let products = [
+            {
+                        title: "Macbook ",
+                        description : "New iMac from USA",
+                        price: 3000,
+                        thumnail : 'macbookpro/image__bhennzolfcwi_large_2x.jpg?'
+            },
+            {
+                        title: "Apple Watch ",
+                        description : "New CPU from USA",
+                        price: 200,
+                        thumnail : 'applewatch/image__bgx6poxiaraq_large_2x.jpg?'
+            },
+            {
+                        title: "iPhone",
+                        description : "New iMac from USA",
+                        price: 1200,
+                        thumnail : 'iphone/image__cnyoqnzvxxaq_large_2x.jpg?'
+            },
+            ,
+            {
+                        title: "iPhone 15 pro ",
+                        description : "New CPU from USA",
+                        price: 1800,
+                        thumnail : 'iphone15proandiphone15promax/image__btmvnyfmx16q_large_2x.jpg?'
+            },
+            {
+                        title: "Macbook Air",
+                        description : "New iMac from USA",
+                        price: 3500,
+                        thumnail : 'macbookair/image__brp6sdbkqjma_large_2x.jpg?'
+            }
+           
+]
+products.map(function product (){
+            console.log(product)
+ })
+console.log(products[0].title)
+
+let SECTION = document.querySelector('section')
+products.map((product) =>{
+            console.log(product)
+            SECTION.innerHTML += `
+            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="p-8 rounded-t-lg" src="${BaseUrl}${product.thumnail}" alt="product image" />
+            </a>
+            <div class="px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${product.title}</h5>
+                </a>
+                <h6 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${product.description}</h6>
+                <div class="flex items-center mt-2.5 mb-5">
+                    <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                    </div>
+                    <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+                </div>
+                <div class="flex items-center justify-between">
+                    <span class="text-xl font-bold text-gray-900 dark:text-white">$ ${product.price}</span>
+                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                </div>
+            </div>
+        </div>
+        `
+})
+ 
+
+
